7b112a3529977d66810fcc9ab007190135d57bc5
Bruno Carvalho da Costa <brunoccst@gmail.com>
2022-08-27 19:23:00 +0200

    Set data json as embedded resource

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using API;
 using DataAccess.Entities;
 using DataAccess.Interfaces;
@@ -11,17 +10,28 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Hosting;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Newtonsoft.Json;
+using System.Reflection;
+using System.Threading.Tasks;
 
 [assembly: WebJobsStartup(typeof(Startup))]
 namespace API
 {
     public class Startup : IWebJobsStartup
     {
+        public async Task<string> GetChatLogTextFromFileAsync() {
+            var assembly = this.GetType().GetTypeInfo().Assembly;
+
+            var resourceStream = assembly.GetManifestResourceStream("API.data.chatLog.json");
+            
+            using var reader = new StreamReader(resourceStream, Encoding.UTF8);
+            return await reader.ReadToEndAsync();
+        }
+
         public void Configure(IWebJobsBuilder builder)
         {
             builder.Services.TryAddScoped(s =>
                 {
-                    string jsonText = System.IO.File.ReadAllText("data.json");
+                    string jsonText = GetChatLogTextFromFileAsync().Result;
                     return JsonConvert.DeserializeObject<List<ChatLog>>(jsonText);
                 }
             );
