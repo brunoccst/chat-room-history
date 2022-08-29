4f90e16a3eb395599f41e2d95b87429042bc5821
Bruno Carvalho da Costa <brunoccst@gmail.com>
2022-08-29 23:27:24 +0200

    Added "alt" prop to GitHub icon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
         <ChatEntryList></ChatEntryList>
       </ChatLogContext.Provider>
       <a href="https://github.com/brunoccst/chat-room-history">
-        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" className='github-icon' />
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" className='github-icon' alt="GitHub icon" />
         <div>chat-room-history</div>
       </a>
     </div>
