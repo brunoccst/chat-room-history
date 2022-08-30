9b5084cbc2f68f639501bd2f628c2179ea21150e
Bruno Carvalho da Costa <brunoccst@gmail.com>
2022-08-30 03:30:14 +0200

    Added missing "alt" prop

--- client/src/components/load-spinner/LoadSpinner.tsx
+++ client/src/components/load-spinner/LoadSpinner.tsx
@@ -3,7 +3,7 @@ import './load-spinner.scss';
 const LoadSpinner = () => {
     return (
         <div className="load-spinner">
-            <img src="https://cdn.icon-icons.com/icons2/2406/PNG/512/spinner_icon_145934.png" />
+            <img src="https://cdn.icon-icons.com/icons2/2406/PNG/512/spinner_icon_145934.png" alt="Load spinner icon"/>
         </div>
     )
 }
