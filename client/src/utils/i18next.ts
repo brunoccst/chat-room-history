903127513a3e97e8f0feba2965a1bbd6ddd77070
Bruno Carvalho da Costa <brunoccst@gmail.com>
2022-09-01 16:07:02 +0200

    Removed react-i18next

--- client/src/utils/i18next.ts
+++ client/src/utils/i18next.ts
@@ -1,9 +1,7 @@
 import i18next from 'i18next';
-import { initReactI18next } from "react-i18next";
 import resources from 'resources';
 
 i18next
-    .use(initReactI18next)
     .init({
         resources: resources,
         fallbackLng: "en",
